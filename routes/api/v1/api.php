8b1c3f434caabaece0ce30cb7eb02d79c0923db3
Zeeshan <hankhan123623@gmail.com>
2026-08-27 20:27:41 +0500

    Fix duplicate payment routes in API

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -144,10 +144,10 @@ Route::group(['namespace' => 'Api\V1', 'middleware'=>['localization','react']], 
 
         Route::put('update-announcment', 'VendorController@update_announcment');
 
-        Route::post('make-collected-cash-payment', 'VendorController@make_payment')->name('make_payment');
-        Route::post('make-wallet-adjustment', 'VendorController@make_wallet_adjustment')->name('make_wallet_adjustment');
+        Route::post('make-collected-cash-payment', 'VendorController@make_payment')->name('vendor_make_payment');
+        Route::post('make-wallet-adjustment', 'VendorController@make_wallet_adjustment')->name('vendor_make_wallet_adjustment');
 
-        Route::get('wallet-payment-list', 'VendorController@wallet_payment_list')->name('wallet_payment_list');
+        Route::get('wallet-payment-list', 'VendorController@wallet_payment_list')->name('vendor_wallet_payment_list');
 
         //Report
         Route::get('get-expense', 'ReportController@expense_report');
